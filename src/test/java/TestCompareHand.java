daa7a733f51d397538c21e07485c53cf0bc9151b
marcus.larsen <marcus.larsen.swe@gmail.com>
2022-08-25 02:11:40 +0200

    hand duel test

---
+++ src/test/java/TestCompareHand.java
@@ -0,0 +1,37 @@
+import org.junit.jupiter.api.Test;
+
+public class TestCompareHand {
+
+    @Test
+    public void testScissorsVSScissors(){
+
+    }
+
+    @Test
+    public void testScissorsVSRock(){
+
+    }
+
+    @Test
+    public void testScissorsVSPaper(){
+
+    }
+
+    @Test
+    public void testRockVSRock(){
+
+    }
+
+    @Test
+    public void testRockVSPaper(){
+
+    }
+
+    @Test
+    public void testPaperVSPaper(){
+
+    }
+    
+
+
+}
--- src/test/java/TestRandom.java
+++ src/test/java/TestRandom.java
@@ -1,26 +1,23 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 
 public class TestRandom {
 
     String hand = GetHand.giveHand();
 
-
     @Test
     public void testRandomReturn() {
-
         assertTrue(hand.equals("SCISSORS") || hand.equals("ROCK")
                 ||hand.equals("PAPER"));
     }
 
-
+    /*
     @Test
     public void testRandomReturnTestofTest() {
         assertTrue(hand.equals("SCIiSSORS") || hand.equals("RoOCK")
                 || hand.equals("PaAPER"));
     }
+    */
 
 }
